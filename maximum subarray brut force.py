c8b55fafb90c83579e34ec6ddc73cba951751308
kishan pandey <kishanpandey5034@gmail.com>
2024-05-20 22:34:37 +0530

    Brute force (TLE error)

--- maximum subarray brut force.py
+++ maximum subarray brut force.py
@@ -1,13 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
-        max_sum = nums[0]
-        current_sum = nums[0]
-        for i in range(1, n):
-            current_sum += nums[i]
-            if current_sum < nums[i]:
-                current_sum = nums[i]
-            if max_sum < current_sum:
-                max_sum = current_sum
+        max_sum = float('-inf')  
+        for i in range(n):
+            current_sum = 0
+            for j in range(i, n):
+                current_sum += nums[j]
+                max_sum = max(max_sum, current_sum)
         return max_sum
-
