3932fc3ee9c47418bd2cce706822e0ac2b3e791d
kishan pandey <kishanpandey5034@gmail.com>
2024-05-20 23:16:57 +0530

    Brute force

--- maximum subarray brut force.py
+++ maximum subarray brut force.py
@@ -8,3 +8,9 @@ class Solution:
                 current_sum += nums[j]
                 max_sum = max(max_sum, current_sum)
         return max_sum
+
+
+"""LOGIC: initialize maxsum to least possible value i.e -infinity , now start iteration fromi= 0 to n and inside there
+another iteration with j from i to n. This will cover every possible sunsequet array psssible.
+No befor j iteration  make current sum=0 as we have already stored maxsum of last iteration.
+hence for all iteration we will get our max sum"""
